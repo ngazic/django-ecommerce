93741fa25029877c4139a4d6f144a5ecbcc4583a
Nihad <ngazic@gmail.com>
2021-09-23 16:52:09 +0200

    added index page, header, footer and static files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Django E-Commerce site
+
+## Development
+```
+mk dir .venv # to create virtual env or $python -m venv
+python -m pip install Django #django installation
+django-admin startproject config . # create django project
+
+
+```
--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print('=========base dir \n', BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_store'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
+# STATICFILES_DIR = [
+#     '/my_store/static/',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
