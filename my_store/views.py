3a42af8e9a8e3ff220a39eebf40f079a563b644f
Nihad <ngazic@gmail.com>
2021-09-27 16:09:51 +0200

    added product detail url and template

--- my_store/views.py
+++ my_store/views.py
@@ -4,17 +4,30 @@ from .models import Product
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'index.html', context)
     
     
 def store(request, category_slug=None):
     products = Product.objects.all().filter(is_available=True)
     if category_slug:
-        print('if condition')
         products = products.filter(category__slug=category_slug)
     context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
-def product_detail(request, category_slug=None, product_slug=None):
-    return render(request, 'index.html')
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Product.DoesNotExist as e:
+        return HttpResponse('Error, product does not exists')
+        raise e
+        
+    context = {
+        'product': single_product
+        }
+    return render(request, 'store/product-detail.html', context)
